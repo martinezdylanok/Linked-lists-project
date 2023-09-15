c767072e79f1a1a49f62af7d81d96f1460116b0b
Lpik97 <dm97fx@gmail.com>
2023-09-15 10:23:45 +0200

    feat(myList object properties tested)
    All the properties inside the myList object were tested in the main file js file.

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-var hello = "World !";
+import linkedList from "./linkedList.js";
+
+const myList = new linkedList();
+myList.append("Append 1");
+myList.append("Append 2");
+myList.append("Append 3");
+myList.prepend("Prepended 1");
+
+console.log("Size:", myList.size);
+console.log("Head:", myList.head.value);
+console.log("Tail:", myList.tail().value);
+
+console.log("Node at index 2:", myList.at(2));
+console.log("Contains Append 9:", myList.containsValue("Append 9"));
+console.log("Index of Append 3:", myList.findValue("Append 3"));
+
+console.log("Inserted at index 2:", myList.insertAt("Inserted node", 2));
+console.log("Node at index 2:", myList.at(2));
+myList.toString();
+
+console.log("Removed at index 2:", myList.removeAt(2));
+myList.toString();
